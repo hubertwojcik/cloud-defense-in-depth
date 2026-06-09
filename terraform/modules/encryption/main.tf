46ff591782b5049da000b8e70becdd936e56a0a6
hubert.wojcik <hwojcikk@gmail.com>
2026-06-09 19:10:40 +0200

    infra: add encryption module

---
+++ terraform/modules/encryption/main.tf
@@ -0,0 +1,29 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_kms_key" "kms_key" {
+    
+    deletion_window_in_days = 10
+
+    enable_key_rotation     = true
+    policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "key-default-1"
+    Statement = [
+      {
+        Sid    = "Enable IAM User Permissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
+        Action   = "kms:*"
+        Resource = "*"
+      },C
+    ]
+  })
+     tags = {
+        Project     = var.project
+        Environment = var.environment
+        ManagedBy   = "terraform"
+        }
+      
+}
