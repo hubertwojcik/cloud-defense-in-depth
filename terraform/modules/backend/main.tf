a4ba31407e15b561a408d3af2978342f45a76131
hubert.wojcik <hwojcikk@gmail.com>
2026-06-09 21:20:08 +0200

    infra: apply checkov fixes

--- terraform/modules/backend/main.tf
+++ terraform/modules/backend/main.tf
@@ -9,6 +9,26 @@ resource "aws_s3_bucket" "terraform_state" {
     }
 }
 
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+    bucket = aws_s3_bucket.terraform_state.id
+
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+    bucket = aws_s3_bucket.terraform_state.id
+
+    rule {
+        apply_server_side_encryption_by_default {
+            sse_algorithm     = "aws:kms"
+            kms_master_key_id = var.kms_key_arn
+        }
+    }
+}
+
 resource "aws_s3_bucket_versioning" "terraform_state" {
     bucket = aws_s3_bucket.terraform_state.id
     versioning_configuration {
