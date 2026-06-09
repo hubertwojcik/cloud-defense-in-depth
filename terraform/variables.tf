4ead19db31b400ce5a122acf3cc6f4649a45de12
hubert.wojcik <hwojcikk@gmail.com>
2026-06-09 19:10:50 +0200

    docs: refactore

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
-variable "project"  {
-    type = string
-    default = "cloud-defense-in-depth"
+variable "project" {
+  type    = string
+  default = "cloud-defense-in-depth"
 }
 
 variable "environment" {
-    type = string
-    default = "dev"
+  type    = string
+  default = "dev"
 }
