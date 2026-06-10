9be4438783cad8812db64aa7f0e922fbdc24df82
hubert.wojcik <hwojcikk@gmail.com>
2026-06-10 20:25:56 +0200

    feat: add aws security hub

---
+++ terraform/security_hub.tf
@@ -0,0 +1,14 @@
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+
+resource "aws_securityhub_account" "main" {}
+
+resource "aws_securityhub_standards_subscription" "cis_v14" {
+  depends_on    = [aws_securityhub_account.main]
+  standards_arn = "arn:aws:securityhub:${data.aws_region.current.name}::standards/cis-aws-foundations-benchmark/v/1.4.0"
+}
+
+resource "aws_securityhub_standards_subscription" "fsbp" {
+  depends_on    = [aws_securityhub_account.main]
+  standards_arn = "arn:aws:securityhub:${data.aws_region.current.name}::standards/aws-foundational-security-best-practices/v/1.0.0"
+}
